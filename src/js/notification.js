ec5fb0a7d05344f59e93360833abd06488bd34c5
Vitaliy <vitaliy-yakovlev@ukr.net>
2021-06-01 17:59:26 +0300

    Убрал лишнее

--- src/js/notification.js
+++ src/js/notification.js
@@ -2,7 +2,6 @@ import '../sass/main.scss';
 import { error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
-
 import '@pnotify/confirm/dist/PNotifyConfirm.css';
 
 export default error;
